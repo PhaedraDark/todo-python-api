c54a47f1c2ba6e6b7cb5e593bc41af21d941ef2d
PhaedraDark <phaedradarkstar@hotmail.co.uk>
2026-06-08 15:38:07 +0100

    fixing format and adjusting id to be an increasing number to avoid issues with duplicate task names

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel
 
 class Task(BaseModel):
     descr: str
-    completed: bool
 
 app = FastAPI()
 
@@ -11,27 +10,30 @@ todo_list = []
 
 @app.get("/tasks")
 def read_list():
+    id = 0
+    for i in range(len(todo_list)):
+        id += 1
+        todo_list[i].update({"id": id})
     return todo_list
 
 @app.post("/tasks")
 def add_task(todo_task: Task):
     task = todo_task.descr
-    todo_list.append({"descr": task, "completed": False})
+    todo_list.append({"id": 0, "descr": task, "completed": False})
     return {"msg": "Task added."}
 
 @app.delete("/tasks/{id}")
-def delete_task(id: str):
+def delete_task(id: int):
     for task in todo_list:
-        if task["descr"] == id:
+        if task["id"] == id:
             todo_list.remove(task)
             return {"msg": "Task removed."}
-        
     return {"msg": "Task not found."}
 
 @app.patch("/tasks/{id}")
-def complete_task(descr: str):
+def complete_task(id: int):
     for task in todo_list:
-        if task["descr"] == descr:
+        if task["id"] == id:
             task["completed"] = not task["completed"]
-
+            return {"msg": "Task completion status changed."}
     return {"msg": "Task not found."}
