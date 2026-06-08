89fa2f2f6da03c30f1ae1f12e12d39b8ede42124
PhaedraDark <phaedradarkstar@hotmail.co.uk>
2026-06-08 15:07:09 +0100

    delete and complete

--- main.py
+++ main.py
@@ -17,4 +17,21 @@ def read_list():
 def add_task(todo_task: Task):
     task = todo_task.descr
     todo_list.append({"descr": task, "completed": False})
-    return "Task added"
+    return {"msg": "Task added."}
+
+@app.delete("/tasks/{id}")
+def delete_task(id: str):
+    for task in todo_list:
+        if task["descr"] == id:
+            todo_list.remove(task)
+            return {"msg": "Task removed."}
+        
+    return {"msg": "Task not found."}
+
+@app.patch("/tasks/{id}")
+def complete_task(descr: str):
+    for task in todo_list:
+        if task["descr"] == descr:
+            task["completed"] = not task["completed"]
+
+    return {"msg": "Task not found."}
