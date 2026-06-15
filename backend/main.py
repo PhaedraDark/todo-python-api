9eb60ba87cac5bb0b882db3cac597b30e7df1837
PhaedraDark <phaedradarkstar@hotmail.co.uk>
2026-06-15 10:52:56 +0100

    origin changed now that frontend page is deployed

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware, 
-    allow_origins=["*"],
+    allow_origins=["https://phaedradark.github.io/todo-python-api/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
