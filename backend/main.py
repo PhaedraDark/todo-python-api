54dc1c239476dd37dbf79d34a90bd66e7ba22ea7
PhaedraDark <115639219+PhaedraDark@users.noreply.github.com>
2026-06-15 12:07:25 +0100

    Revert "backend stuff deleted from frontend branch"
    This reverts commit f5d902d46c0d936d9b6a535a1ea696b0286da98f.

---
+++ backend/main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+class Task(BaseModel):
+    descr: str
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+todo_list = []
+
+@app.get("/tasks")
+def read_list():
+    return todo_list
+
+@app.post("/tasks")
+def add_task(todo_task: Task):
+    next_id = todo_list[-1]["id"] + 1 if todo_list else 1
+    todo_list.append({"id": next_id, "descr": todo_task.descr, "completed": False})
+    return {"msg": "Task added."}
+
+@app.delete("/tasks/{id}")
+def delete_task(id: int):
+    for task in todo_list:
+        if task["id"] == id:
+            todo_list.remove(task)
+            return {"msg": "Task removed."}
+    return {"msg": "Task not found."}
+
+@app.patch("/tasks/{id}")
+def complete_task(id: int):
+    for task in todo_list:
+        if task["id"] == id:
+            task["completed"] = not task["completed"]
+            return {"msg": "Task completion status changed."}
+    return {"msg": "Task not found."}
